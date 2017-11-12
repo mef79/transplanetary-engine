6ab043f588d96c66ce5e9d82062f161235324878
Phillip Dodson <clevergadget@gmail.com>
2017-11-11 19:39:38 -0500

    Showing title and initial stitch name

--- app/containers/TitlePage/index.js
+++ app/containers/TitlePage/index.js
@@ -3,19 +3,32 @@
  * TitlePage
  *
  */
-
+import styled from 'styled-components'
 import React, { PropTypes } from 'react'
 import { connect } from 'react-redux'
 import { createStructuredSelector } from 'reselect'
-import { selectStoryTitle, selectInitialStitchName} from '../Game/selectors'
+import { selectStoryTitle, selectInitialStitchName } from '../Game/selectors'
 
 export class TitlePage extends React.PureComponent { // eslint-disable-line react/prefer-stateless-function
   render() {
+    const Title = styled.h1`
+      font-size:1.2em;
+
+    `
+    const PageLayout = styled.div`
+      display: flex;
+      flex-direction: column;
+      align-items: center;
+      justify-content: space-around;
+    `
     return (
-      <div>
-        {this.PropTypes.title}
-        {this.PropTypes.initialStitchName}
-      </div>
+
+      <PageLayout>
+        <Title>
+          {this.props.title}
+        </Title>
+        {this.props.initialStitchName}
+      </PageLayout>
     )
   }
 }
