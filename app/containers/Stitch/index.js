425ce5a811fcaddc42f8b037b699d8fd3b84b961
Meaghan Fenelon <mefenelon@gmail.com>
2018-03-24 16:28:09 -0400

    Handle flag-based portions of text

--- app/containers/Stitch/index.js
+++ app/containers/Stitch/index.js
@@ -8,12 +8,73 @@ import React, { PropTypes } from 'react'
 import { connect } from 'react-redux'
 import { createStructuredSelector } from 'reselect'
 import TextFormatter from 'components/TextFormatter'
+import { selectFlags } from 'containers/Game/selectors'
 
 export class Stitch extends React.PureComponent { // eslint-disable-line react/prefer-stateless-function
+  setFlagBasedSnippets = content => {
+    const flagRegex = /\{(.+?):(.+?)(\|(.+?))?\}/
+    const match = flagRegex.exec(content)
+
+    if (match) {
+      const wholeExpression = match[0]
+      const flagCondition = match[1]
+      const ifFlag = match[2]
+      const ifNotFlag = match[4] ? match[4] : ''
+
+      const comparisonStrings = ['<=', '>=', '=', '<', '>']
+
+      let comparison
+      let flagName = flagCondition
+      let flagValue = true
+
+      comparisonStrings.forEach(s => {
+        if (flagCondition.indexOf(s) > -1 && !comparison) {
+          flagName = flagCondition.split(s)[0].trim()
+          flagValue = flagCondition.split(s)[1].trim()
+          comparison = s
+        }
+      })
+
+      const currentFlag = this.props.flags.toJS()[flagName]
+
+      let formattedContent
+
+      if (flagValue === true) {
+        formattedContent = currentFlag ? content.replace(wholeExpression, ifFlag) : formattedContent = content.replace(wholeExpression, ifNotFlag)
+      }
+
+      else {
+        switch (comparison) {
+          case '<=':
+            formattedContent = currentFlag <= flagValue ? content.replace(wholeExpression, ifFlag) : content.replace(wholeExpression, ifNotFlag)
+            break
+          case '>=':
+            formattedContent = currentFlag >= flagValue ? content.replace(wholeExpression, ifFlag) : content.replace(wholeExpression, ifNotFlag)
+            break
+          case '=':
+            formattedContent = currentFlag === flagValue ? content.replace(wholeExpression, ifFlag) : content.replace(wholeExpression, ifNotFlag)
+            break
+          case '<':
+            formattedContent = currentFlag < flagValue ? content.replace(wholeExpression, ifFlag) : content.replace(wholeExpression, ifNotFlag)
+            break
+          case '>':
+            formattedContent = currentFlag > flagValue ? content.replace(wholeExpression, ifFlag) : content.replace(wholeExpression, ifNotFlag)
+            break
+          default:
+            formattedContent = content
+            break
+        }
+      }
+
+      return this.setFlagBasedSnippets(formattedContent)
+    }
+    return content
+  }
+
   render() {
     return (
       <div id="stitchContent">
-        <TextFormatter content={this.props.stitch.get('content').get(0)} />
+        <TextFormatter content={this.setFlagBasedSnippets(this.props.stitch.get('content').get(0))} />
       </div>
     )
   }
@@ -22,9 +83,12 @@ export class Stitch extends React.PureComponent { // eslint-disable-line react/p
 Stitch.propTypes = {
   stitch: PropTypes.object.isRequired,
   dispatch: PropTypes.func.isRequired,
+  flags: PropTypes.object,
 }
 
-const mapStateToProps = createStructuredSelector({})
+const mapStateToProps = createStructuredSelector({
+  flags: selectFlags(),
+})
 
 function mapDispatchToProps(dispatch) {
   return {
