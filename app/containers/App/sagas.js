4eb716b2efc26a4dda7dc391572a80d6bbbd5b50
Meaghan Fenelon <mefenelon@gmail.com>
2018-05-19 17:29:35 -0400

    Allow for setting different music when hitting certain stitches

--- app/containers/App/sagas.js
+++ app/containers/App/sagas.js
@@ -1,20 +1,27 @@
 import { takeEvery, takeLatest, put, select } from 'redux-saga/effects'
-import { playSound, setPlayingSFX } from './actions'
+import { playSound, setPlayingSFX, setPlayingMusic } from './actions'
 import { SET_PLAYING_SFX, SET_PLAYING_MUSIC } from './constants'
 import { SET_CURRENT_CONTEXT } from 'containers/Game/constants'
 import { getPlayingSFX, getPlayingMusic } from './selectors'
 import { selectGameDomain } from 'containers/Game/selectors'
+import musicTransitions from '../../ink/music.json'
+import { fromJS } from 'immutable'
 
 // Individual exports for testing
 export function* defaultSaga() {
   // See example in containers/HomePage/sagas.js
 }
 
-export function* handleDecisions() {
+export function* handleDecisions(action) {
   // when decisions are made, they must fire an action to update an object containing all decisions
   const gameData = yield select(selectGameDomain())
   const gameDataJS = gameData.toJS()
-  const music = yield select(getPlayingMusic())
+  let music = yield select(getPlayingMusic())
+  const transitions = fromJS(musicTransitions)
+  if (transitions.has(action.stitchName)) {
+    music = transitions.get(action.stitchName)
+    yield put(setPlayingMusic(music))
+  }
   localStorage.setItem('visibleStitches', JSON.stringify(gameDataJS.visibleStitches))
   localStorage.setItem('currentStitch', JSON.stringify(gameDataJS.currentStitch))
   localStorage.setItem('options', JSON.stringify(gameDataJS.options))
